9b970809fc947fa1473770f044715e4203bf9dcd
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2016-02-22 12:14:54 +0000

    Game name changed in LICENSE headers.

--- src/sandwiches/sandwich.hpp
+++ src/sandwiches/sandwich.hpp
@@ -1,20 +1,20 @@
 /***
-    Copyright 2013 - 2016 Moises J. Bonilla Caraballo (Neodivert)
+    Copyright 2013 - 2016 Moises J. Bonilla Caraballo (moisesjbc)
 
-    This file is part of JDB.
+    This file is part of sandwiches-game.
 
-    JDB is free software: you can redistribute it and/or modify
+    sandwiches-game is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    JDB is distributed in the hope that it will be useful,
+    sandwiches-game is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with JDB.  If not, see <http://www.gnu.org/licenses/>.
+    along with sandwiches-game.  If not, see <http://www.gnu.org/licenses/>.
  ***/
 
 #ifndef SANDWICH_HPP
