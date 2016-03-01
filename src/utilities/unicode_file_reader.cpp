55e86d8d636dd5c8fdc2f42827bb0c540ef34e37
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2016-03-01 16:46:18 +0000

    Added missing license headers.

--- src/utilities/unicode_file_reader.cpp
+++ src/utilities/unicode_file_reader.cpp
@@ -1,3 +1,22 @@
+/***
+    Copyright 2013 - 2016 Moises J. Bonilla Caraballo (moisesjbc)
+
+    This file is part of sandwiches-game.
+
+    sandwiches-game is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    sandwiches-game is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with sandwiches-game.  If not, see <http://www.gnu.org/licenses/>.
+ ***/
+
 #include <utilities/unicode_file_reader.hpp>
 #include <locale>
 #include <fstream>
