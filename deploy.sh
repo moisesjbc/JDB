064dfb7e3f6ab0ff62f0e4a24dfa50faab9e96ef
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2016-03-11 19:12:45 +0000

    Fixed permissions errors when generating packages.

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Build DEB package for Ubuntu
-make clean &&
-sudo rm -r CMakeCache.txt CMakeFiles &&
+make clean
+sudo rm -r CMakeCache.txt CMakeFiles _CPack_Packages
 cmake -DCMAKE_BUILD_TYPE=Release . &&
 sudo make -j $(nproc) package
 
 # Build ZIP package for Windows
-make clean &&
-sudo rm -r CMakeCache.txt CMakeFiles &&
+make clean
+sudo rm -r CMakeCache.txt CMakeFiles CPack_Packages
 i686-w64-mingw32.static-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_USING_MXE=1 . &&
 sudo make -j $(nproc) package
