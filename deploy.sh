40424eeb7c7db616d843b492f9899bd44ca065cb
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2016-03-11 18:07:10 +0000

    Added sudo in deployment script.

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 make clean &&
 sudo rm -r CMakeCache.txt CMakeFiles &&
 cmake -DCMAKE_BUILD_TYPE=Release . &&
-make -j $(nproc) package
+sudo make -j $(nproc) package
 
 # Build ZIP package for Windows
 make clean &&
 sudo rm -r CMakeCache.txt CMakeFiles &&
 i686-w64-mingw32.static-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_USING_MXE=1 . &&
-make -j $(nproc) package
+sudo make -j $(nproc) package
